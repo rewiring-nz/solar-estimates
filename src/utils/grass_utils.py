1de8c5992f5e3acd5d02164c30452cdf8c22b6d1
Cameron Shorter <cameron.shorter@gmail.com>
2025-11-23 06:01:56 +0000

    #58 Ensure grass runs in text instead of GUI mode

--- src/utils/grass_utils.py
+++ src/utils/grass_utils.py
@@ -36,7 +36,7 @@ def setup_grass(
     # Create location if it doesn't exist
     location_path = os.path.join(grassdata_dir, location)
     if not os.path.exists(location_path):
-        cmd = ["grass", "-c", "EPSG:2193", location_path]
+        cmd = ["grass", "--text", "-c", "EPSG:2193", location_path]
         print(f"DEBUG: Attempting to create GRASS Location: {' '.join(cmd)}")
         
         proc = subprocess.Popen(
